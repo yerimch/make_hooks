cbf8f12ab1d24072b7f624b102ca83038199c5a1
yerimch <70640762+yerimch@users.noreply.github.com>
2021-01-04 16:35:07 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>{item}</h1>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+};
+
+/*
+  class App extends React.Component{
+  state={
+    cnt:1
+  };
+  incrementItem =()=>{
+    this.setState({cnt:this.state.cnt+1});
+  }
+  decrementItem=()=>{
+    this.setState({cnt:this.state.cnt-1});
+  }
+  render(){
+    return(
+      <div>
+      <h1>{this.state.cnt}</h1>
+      <button onClick={this.incrementItem}>Increment</button>
+      <button onClick={this.decrementItem}>Decrement</button>
+      </div>
+    );
+  };
+}
+*/
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
