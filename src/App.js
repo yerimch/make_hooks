82aeaa385cc74f8a963a86bea8774c73cdaae090
yerimch <70640762+yerimch@users.noreply.github.com>
2021-01-06 13:43:09 +0900

    useFadeIn

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-const useBeforeLeave = (onBefore) => {
-  if (typeof onBefore !== "function") {
+const useFadeIn = (duration = 1, delay = 0) => {
+  if (typeof duration !== "number" || typeof delay !== "number") {
     return;
   }
-  const handle = (event) => {
-    const { clientY } = event;
-    if (clientY <= 0) onBefore();
-  };
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
-  }, []);
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  });
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => {
-    console.log("pls dont leave");
-  };
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 10);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 {...fadeInH1}>Hello</h1>
+      <p>lalala</p>
     </div>
   );
 };
