8c2a89754b667634af8603fed7f17ea5e8faab51
yerimch <70640762+yerimch@users.noreply.github.com>
2021-01-06 14:43:55 +0900

    useScroll

--- src/App.js
+++ src/App.js
@@ -2,34 +2,23 @@ import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
-const useNetwork = (onChange) => {
-  const [status, setStatus] = useState(navigator.onLine);
-  const handleChange = () => {
-    if (typeof onChange === "function") {
-      onChange(navigator.onLine);
-    }
-    setStatus(navigator.onLine);
+const useScroll = () => {
+  const [state, setState] = useState({ x: 0, y: 0 });
+  const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
   };
   useEffect(() => {
-    window.addEventListener("online", handleChange);
-    window.addEventListener("offline", handleChange);
-    () => {
-      window.removeEventListener("online", handleChange);
-      window.removeEventListener("offline", handleChange);
-    };
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onscroll);
   }, []);
-  return status;
+  return state;
 };
 
 const App = () => {
-  const handleNetworkChange = (online) => {
-    console.log(online ? "We just went online" : "We are offline");
-  };
-  const online = useNetwork(handleNetworkChange);
-
+  const { y } = useScroll();
   return (
-    <div className="App">
-      <h1>{online ? "Online" : "Offline"}</h1>
+    <div className="App" style={{ height: "1000vh " }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
     </div>
   );
 };
